c615bb822bbd5fd3d13df18e583d115519c145d7
om kachhadiya <kachhadiyaom2005@gmail.com>
2026-04-25 11:23:17 +0530

    Add the React Router Dom and React Icons and Add Pages and Compnents.

---
+++ client/src/components/UserHeader.jsx
@@ -0,0 +1,14 @@
+import { VStack, Box } from '@chakra-ui/react'
+import React from 'react'
+
+const UserHeader = () => {
+  return (
+    <VStack>
+      <Box>1</Box>
+      <Box>2</Box>
+      <Box>3</Box>
+    </VStack>
+  )
+}
+
+export default UserHeader
