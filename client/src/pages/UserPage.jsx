d9d04a2642d4b2f37aadc412a688abc17f284a77
om kachhadiya <kachhadiyaom2005@gmail.com>
2026-04-25 17:06:29 +0530

    Add the PostPage page and Action,Comment and UserPost components.

--- client/src/pages/UserPage.jsx
+++ client/src/pages/UserPage.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import UserHeader from '../components/UserHeader'
+import UserPost from '../components/UserPost'
 
 const UserPage = () => {
   return (
     <>
       <UserHeader/>
+      <UserPost postImg={"/post1.png"} postTitle={"My first Thread."} likes={123} replies={10}/>
+      <UserPost postImg={"/post2.png"} postTitle={"Awesome concentration."} likes={13} replies={110}/>
     </>
   )
 }
